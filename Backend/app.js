8078d5b4d05b61b7261fdaecdd6199582d0e8ce4
ArpitKrRespect <arpitkr2020@gmail.com>
2025-01-08 02:59:20 +0530

    DataBase Connected

--- Backend/app.js
+++ Backend/app.js
@@ -3,9 +3,11 @@ dotenv.config();
 import express from "express";
 import cors from "cors"; 
 
+import connectDB from "./db/db.js";
 const app = express();
 
 app.use(cors());
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
